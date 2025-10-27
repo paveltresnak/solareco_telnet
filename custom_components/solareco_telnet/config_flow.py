80d2232fc47cff0f8097ad59244e044f306adeba
Pavel Třešňák <pavel@tresnakovi.cz>
2025-10-27 08:09:42 +0100

    Update config_flow.py

--- custom_components/solareco_telnet/config_flow.py
+++ custom_components/solareco_telnet/config_flow.py
@@ -96,10 +96,6 @@ class SolarEcoTelnetConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 class OptionsFlowHandler(config_entries.OptionsFlow):
     """Handle options flow for SolarEco Telnet."""
 
-    def __init__(self, config_entry):
-        """Initialize options flow."""
-        self.config_entry = config_entry
-
     async def async_step_init(self, user_input=None):
         """Manage the options."""
         if user_input is not None:
